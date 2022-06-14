75068d16d4749bdb2eb7e18eb0a9b98c911d8fac
Kevin Zheng <kevzheng27@gmail.com>
2022-06-13 22:35:10 -0400

    commit tree 4154cb9505df3c9f379daa423fa6fa5b73c92c8f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(name, event) {
+    const newarray = [];
+    for (let n = 0; n < name.length; n++) {
+        newarray.push(`Thank you, ${name[n]}, for the wonderful ${event} gift!`); }
+    return newarray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+}
